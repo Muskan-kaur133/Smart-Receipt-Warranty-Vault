5f7f8e9f98b84433c1208169ee4767a0de3bbcb4
Muskan Kaur <mk9977943@gmail.com>
2026-02-09 19:26:16 +0530

    Add WarrantyItem model

---
+++ app/src/main/java/com/example/warrantyvault/model/WarrantyItem.java
@@ -0,0 +1,10 @@
+package com.example.warrantyvault.model;
+
+public class WarrantyItem {
+    public int id;
+    public String productName;
+    public long purchaseDate;
+    public int warrantyMonths;
+    public String receiptImagePath;
+    public long warrantyEndDate;
+}
