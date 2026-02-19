666bf28b46c0bef69685796f761d62283229d800
Muskan Kaur <mk9977943@gmail.com>
2026-02-19 21:52:19 +0530

    made the landing page

---
+++ app/src/main/java/com/example/warrantyvault/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.warrantyvault;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        TextView tvDateTime = findViewById(R.id.tvDateTime);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy  |  hh:mm a", Locale.getDefault());
+        tvDateTime.setText(sdf.format(new Date()));
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }, 5000);
+    }
+}
